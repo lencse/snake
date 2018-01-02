038b5349da5867d26b60f28d35688b89b789ad38
Levente Löki <leventeloki@gmail.com>
2018-01-02 20:29:57 +0100

    RES FCKN PONSIVE

--- src/Browser/Painter.ts
+++ src/Browser/Painter.ts
@@ -6,8 +6,8 @@ export default class Painter {
     private context: CanvasRenderingContext2D
 
     constructor(canvas: HTMLCanvasElement) {
-        canvas.setAttribute('width', '800')
-        canvas.setAttribute('height', '800')
+        canvas.setAttribute('width', '2400')
+        canvas.setAttribute('height', '2400')
         this.context = canvas.getContext('2d')
     }
 
@@ -21,7 +21,7 @@ export default class Painter {
         for (let row = 1; row <= game.map.height; ++row) {
             for (let col = 1; col <= game.map.width; ++col) {
                 this.context.fillStyle = 'x' === game.map.cell(pos(row, col)) ? '#900' : '#000'
-                this.context.fillRect((col - 1) * 35, (row - 1) * 35, 35, 35)
+                this.context.fillRect((col - 1) * 100, (row - 1) * 100, 100, 100)
             }
         }
     }
@@ -29,7 +29,7 @@ export default class Painter {
     private paintPill(game: Game) {
         if (game.pill) {
             this.context.fillStyle = '#199'
-            this.context.arc((game.pill.column - 1) * 35 + 17.5, (game.pill.row - 1) * 35 + 17.5, 13, 0, 2*Math.PI)
+            this.context.arc((game.pill.column - 1) * 100 + 50, (game.pill.row - 1) * 100 + 50, 30, 0, 2*Math.PI)
             this.context.fill()
         }
     }
@@ -38,7 +38,7 @@ export default class Painter {
         this.context.fillStyle = '#191'
         game.snake.positions.forEach((position: Position) => {
             this.context.beginPath()
-            this.context.arc((position.column - 1) * 35 + 17.5, (position.row - 1) * 35 + 17.5, game.snake.head.equals(position) ? 14.5 : 17, 0, 2*Math.PI)
+            this.context.arc((position.column - 1) * 100 + 50, (position.row - 1) * 100 + 50, game.snake.head.equals(position) ? 38 : 45, 0, 2*Math.PI)
             this.context.fill()
         })
     }
